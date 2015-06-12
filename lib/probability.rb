090e08be667e439011688257a528ed4d20d335a6
Austin <Schaefer.Austin.P@gmail.com>
2015-06-12 13:11:21 -0400

    Removed duplicate probability.rb file created by vim.

--- lib/probability.rb
+++ lib/probability.rb
@@ -3,6 +3,6 @@ require "probability/version"
 require "probability/core_ext/integer"
 require "probability/core_ext/float"
 
-#TODO: Wrap core_ext includes into generalized form?
+#TODO: Wrap core_ext includes into more generalized form?
 module Probability
 end
