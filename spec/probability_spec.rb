aa37bb2858def04264bac7c752cfe09f14a2685e
Enrique Esquinas <esquinas.enrique@gmail.com>
2017-02-28 15:37:43 +0100

    Update specs.
    Otherwise `$ rake spec` fails.

--- spec/probability_spec.rb
+++ spec/probability_spec.rb
@@ -1,27 +1,24 @@
-require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Probability for Integer" do
-  context "1.in(1)" do
-    subject { 1.in(1) }
-    specify { should be_true }
+describe 'Probability for Integer' do
+  it 'returns true when P=1' do
+    expect(1.in(1)).to be_truthy
   end
 
-  context "0.in(1)" do
-    subject { 0.in(1) }
-    specify { should be_false }
+  it 'returns false when P=0' do
+    expect(0.in(1)).to be_falsey
   end
 
-  context "3.in(0)" do
-    subject { 3.in(0) }
-    specify { should be_false }
+  it 'returns false when 3.in 0' do
+    expect(3.in(0)).to be_falsey
   end
 
-  context "with blocks" do
-    it "yields the block for 1.in(1)" do
+  context 'with blocks' do
+    it 'yields the block for 1.in(1)' do
       expect { |b| 1.in(1, &b) }.to yield_with_no_args
     end
 
-    it "does not yield the block for 0.in(1)" do
+    it 'does not yield the block for 0.in(1)' do
       expect { |b| 0.in(1, &b) }.to_not yield_with_no_args
     end
   end
